7427b91114bb27f8aee6b0e730ead7f59d120180
OlegKrikun <oleg.krikun@gmail.com>
2019-11-07 05:05:58 +0300

    Prepare 0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ru.krikun.commonmark"
-version = "0.0.3"
+version = "0.1.0"
 
 val properties = properties("bintray.properties")
 val bintrayUser: String = properties.getProperty("user")

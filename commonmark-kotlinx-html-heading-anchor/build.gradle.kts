5b25d16e7f437e62f63ef7b7ae9ba6e55f57b4f8
OlegKrikun <oleg.krikun@gmail.com>
2020-01-18 21:48:12 +0300

    Provide some dependencies to artifact consumers

--- commonmark-kotlinx-html-heading-anchor/build.gradle.kts
+++ commonmark-kotlinx-html-heading-anchor/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":commonmark-kotlinx-html"))
+    api(project(":commonmark-kotlinx-html"))
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.12")
 
-    implementation("com.atlassian.commonmark:commonmark-ext-heading-anchor:0.13.1")
+    api("com.atlassian.commonmark:commonmark-ext-heading-anchor:0.13.1")
 
     testImplementation(project(":commonmark-kotlinx-html-test"))
 }
--- commonmark-kotlinx-html/build.gradle.kts
+++ commonmark-kotlinx-html/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.12")
 
-    implementation("com.atlassian.commonmark:commonmark:0.13.1")
+    api("com.atlassian.commonmark:commonmark:0.13.1")
 
     testImplementation(project(":commonmark-kotlinx-html-test"))
 }
